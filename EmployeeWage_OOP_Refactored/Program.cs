78805d23f8af273f3dd6ef31cb1304226f8c2e13
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-21 17:30:05 +0530

    Added: Code for employee wage

---
+++ EmployeeWage_OOP_Refactored/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_OOP_Refactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Management System!");
+        }
+    }
+}
---
+++ UC9_MultipleCompanies/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace UC9_MultipleCompanies
+{
+    class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        //public const int EMP_RATE_PER_HOUR = 20;
+        //public const int NUM_OF_WORKING_DAYS = 2;
+        //public const int MAX_HRS_IN_MONTH = 10;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+
+            //Variables
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for Company: " + company + "is:" +totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMart",20,2,10);
+            computeEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
+
+
